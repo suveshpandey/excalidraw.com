838fe7b27f4400a83d4f989493cf5a1bbcb42fb3
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-05-24 12:29:49 +0000

    Chages: changes the http-backend url and ws-backend url to live urls in frontend

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,5 @@
-export const HTTP_BACKEND = "http://localhost:8080/api/v1/user";
-export const WS_BACKEND = "http://localhost:8081";
+// export const HTTP_BACKEND = "http://localhost:8080/api/v1/user";
+// export const WS_BACKEND = "http://localhost:8081";
+
+export const HTTP_BACKEND = "https://excaliboard-backend.onrender.com/api/v1/user";
+export const WS_BACKEND = "https://excaliboard-ws-backend.onrender.com";
