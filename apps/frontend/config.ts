466137b3649bef2c0770c528ff97c2369107adb3
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-05-26 06:31:01 +0000

    Fix: Fixed the ws-backend problem of not working

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -2,4 +2,4 @@
 // export const WS_BACKEND = "http://localhost:8081";
 
 export const HTTP_BACKEND = "https://excaliboard-backend.onrender.com/api/v1/user";
-export const WS_BACKEND = "wss://excaliboard-ws-backend.onrender.com/";
+export const WS_BACKEND = "wss://excaliboard-ws-backend-2.onrender.com";
