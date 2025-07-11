1bd3bca2eeed127962c0db003b4c998a187f4105
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-07-11 17:46:19 +0530

    fixed backend urls in frontend

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,5 +1,5 @@
-export const HTTP_BACKEND = "http://localhost:8080/api/v1/user";
-export const WS_BACKEND = "http://localhost:8081";
+// export const HTTP_BACKEND = "http://localhost:8080/api/v1/user";
+// export const WS_BACKEND = "http://localhost:8081";
 
-// export const HTTP_BACKEND = "https://excaliboard-backend.onrender.com/api/v1/user";
-// export const WS_BACKEND = "wss://excaliboard-ws-backend-2.onrender.com";
+export const HTTP_BACKEND = "https://excaliboard-backend.onrender.com/api/v1/user";
+export const WS_BACKEND = "wss://excaliboard-ws-backend-2.onrender.com";
